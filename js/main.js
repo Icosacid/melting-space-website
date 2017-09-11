6a74fa74fd1115923793ba589eb9e0b6aaac76f1
Alexandre Andrieux <alex@icosacid.com>
2017-09-11 22:16:36 +0200

    Base routing mechanics with Vue.js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+	
+	const HomeComponent = { template: '<div>Home component</div>' }
+	const BlogComponent = { template: '<div>Blog component</div>' }
+	const ContactComponent = { template: '<div>Contact component</div>' }
+	const UIKitComponent = { template: '<div>UIKit component</div>' }
+	
+	const routes = [
+	  { path: '/', component: HomeComponent },
+	  { path: '/blog', component: BlogComponent },
+	  { path: '/contact', component: ContactComponent },
+	  { path: '/ui-kit', component: UIKitComponent }
+	]
+	
+	const router = new VueRouter({
+	  routes // short for `routes: routes`
+	})
+	
+	const app = new Vue({
+	  router
+	}).$mount('#melting-app')
+
+});
