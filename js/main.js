a58304a0c807eadedc61424e0636c675d98398e2
Alex Andrix <alex@icosacid.com>
2018-06-25 17:09:36 +0200

    Fix ref toggle bug and add date field to exercises

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
 						sourceLink: 'http://ravenkwok.com/1194d/',
 						description: 'Spaces and non-spaces working as anti-volumes arranged in a ordrered manner',
 						sourceImageLink: 'http://farm6.staticflickr.com/5467/9809073965_52e39099fa_z.jpg',
+						date: 'May 2017',
 						responsePaths: [
 							'ex-0-jeanf.jpg',
 							'ex-0-yngvesin.gif',
@@ -22,24 +23,60 @@ document.addEventListener('DOMContentLoaded', function() {
 						sourceLink: '',
 						description: 'The beginning of destruction. (cf twin peaks)',
 						sourceImageLink: 'img/ex-1-ref.png',
+						date: 'Oct. 2017',
 						responsePaths: [
 							'ex-1-jeanf.jpg',
 							'',
 							'ex-1-alexandrix.gif'
 						]
 					},
+					/*
 					{
 						sourceLink: '',
 						description: 'Straight despair',
 						sourceImageLink: 'img/ex-2-ref.png',
+						date: 'Oct. 2017',
 						responsePaths: [
 							'',
 							'',
 							''
 						]
 					}
+					*/
 				]
 			}
+		},
+		mounted: function() {
+			// Details animation with jQuery (had to)
+			// Here listeners are killed and rebuilt when you click on the page link
+			// Not optimal but totally cool for this lil' website lol
+			jQuery('.ref-collapse').off().on('click', function() {
+				var $me = jQuery(this),
+					$myRef = $me.siblings('.ref');
+				if ($me.hasClass('open')) {
+					// Collapse
+					$me.removeClass('open');
+					$myRef.animate({
+						opacity: 0
+					}, 150, function() {
+						$myRef.animate({
+							height: 0
+						}, 250);
+					});
+				}
+				else {
+					// Expand
+					$me.addClass('open');
+					$myRef.animate({
+						height: 250
+					}, 250, function() {
+						$myRef.animate({
+							opacity: 1
+						}, 150);
+					});
+				}
+
+			});
 		}
 	}
 	const ContactComponent = { template: jQuery('#contact').html() }
@@ -62,34 +99,5 @@ document.addEventListener('DOMContentLoaded', function() {
 		router,
 		el: '#melting-app'
 	});//.$mount('#melting-app')
-	
-	// Details animation with jQuery (had to)
-	jQuery('.ref-collapse').click(function() {
-		var $me = jQuery(this),
-			$myRef = $me.siblings('.ref');
-		if ($me.hasClass('open')) {
-			// Collapse
-			$me.removeClass('open');
-			$myRef.animate({
-			    opacity: 0
-			}, 150, function() {
-				$myRef.animate({
-				    height: 0
-				}, 250);
-			});
-		}
-		else {
-			// Expand
-			$me.addClass('open');
-			$myRef.animate({
-			    height: 250
-			}, 250, function() {
-				$myRef.animate({
-				    opacity: 1
-				}, 150);
-			});
-		}
-
-	});
 
 });
